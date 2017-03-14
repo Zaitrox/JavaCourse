791988c2e918392513efcf078b5a1f30fbdcfd07
Zaitrox <zelim.anzorov@gmail.com>
2017-03-14 17:41:50 +0100

    Add class BankAccount

---
+++ src/BankAccount.java
@@ -0,0 +1,12 @@
+public class BankAccount {
+    private int accountNumber;
+    private double balance;
+
+    public int getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public double getBalance() {
+        return this.balance;
+    }
+}
